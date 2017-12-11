a69aa5624fcb2781438e60c33829c2caacf0573b
kees2 <kuijl95@gmail.com>
2017-12-11 16:32:34 +0100

    uart rules

--- project_domotica/project_domotica/Debug/main.d
+++ project_domotica/project_domotica/Debug/main.d
@@ -1,5 +1,34 @@
-main.d main.o: .././main.c .././gpio.h .././timer.h
+main.d main.o: .././main.c .././gpio.h .././timer.h .././usart.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h \
+ d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h
 
 .././gpio.h:
 
 .././timer.h:
+
+.././usart.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdio.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\inttypes.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdint.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\stdint.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stdarg.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\lib\gcc\avr\5.4.0\include\stddef.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\util\delay_basic.h:
+
+d:\7.0\toolchain\avr8\avr8-gnu-toolchain\avr\include\math.h:
--- project_domotica/project_domotica/main.c
+++ project_domotica/project_domotica/main.c
@@ -7,12 +7,49 @@
 
 #include "gpio.h"
 #include "timer.h"
+#include "usart.h"
+#include <inttypes.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#define LED_BIT PD5
 
 int main(void)
 {
-	set_output_gpio(0xB, 7);
-    while (1) 
-    {
-    }
+	
+	uint8_t i = 0;
+	uint16_t c = 0;
+
+	DDRD |= (1 << LED_BIT);
+	
+	for (i = 0; i < 5; i++) {
+		PORTD |= (1 << LED_BIT);
+		_delay_ms(50);
+		PORTD &= ~(1 << LED_BIT);
+		_delay_ms(50);
+	}
+
+	init_uart();
+
+	sei();
+
+	while (1) {
+
+		c = uart_getc();
+		if (c == UART_NO_DATA) {
+			uart_putc('A');
+		}
+		else {
+			uart_putc(c);
+		}
+
+		PORTD |= (1 << LED_BIT);
+		_delay_ms(200);
+		PORTD &= ~(1 << LED_BIT);
+		_delay_ms(800);
+	}
+
+	return 0;
 }
 
