c7c5e178313ec4f5e2dae4a3f5259e12a09d6c77
StanGeitel <stan@geitel.nl>
2017-12-14 22:23:39 +0100

    up

--- raspberry/uart.c
+++ raspberry/uart.c
@@ -6,4 +6,19 @@ void init_uart(){
   UART_LCRH |= (1 << 1);  //enable parity
   UART_LCRH |= (1 << 2);  //select even parity
   UART_LCRH |= (3 << 5);  //set word lenght to 8 bits
+
+  UART_CR &= ~(0xFFFF);   //all 16 bits low
+  UART_CR |= (1 << 0);    //uart enabled
+  UART_CR |= (1 << 8);    //enable tx
+  UART_CR |= (1 << 9);    //enable rx
+
+
+}
+
+unsigned char read_uart(){
+  return(UART_DR & 0xFF);
+}
+
+void write_uart(unsigned char data){
+  UART_DR |= data;
 }
--- raspberry/uart.h
+++ raspberry/uart.h
@@ -1,5 +1,8 @@
 #ifndef UART_H_
 #define UART_H_
 
+void init_uart();
+unsigned char read_uart();
+void write_uart(unsigned char data);
 
 #endif
